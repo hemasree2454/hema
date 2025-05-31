203941c6713252d347c70601a5757404ce7b2567
hemasree2454 <hemasreephs@gmail.com>
2025-05-31 12:23:31 +0530

    Add files via upload

---
+++ class.js
@@ -0,0 +1,13 @@
+function printall(){
+   
+        var numberthree=document.getElementById("name").value 
+      var numberfour=document.getElementById("value").value 
+      var numberfive=document.getElementById("number").value 
+      
+    document.getElementById("valuesare").innerHTML=numberthree
+    document.getElementById("num").innerHTML=numberfour
+    document.getElementById("numbers").innerHTML=numberfive
+    document.getElementById("six").innerHTML=numberfour*numberfive
+
+    
+}
